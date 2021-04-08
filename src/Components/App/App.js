f54f6609c57d99d058c2c8be24ab1bb0fa883dfc
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-04-08 15:52:19 -0700

    setup files css and components,

---
+++ src/Components/App/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+// import SearchBar from './SearchBar.js';
+// import SearchResults from './SearchResults.js';
+// import playlist from './Playlist.js';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div>
+					<h1>
+						Ja<span className="highlight">mmm</span>ing
+					</h1>
+					<div className="App">
+						{/* <!-- Add a SearchBar component --> */}
+						<div className="App-playlist">
+							{/* <!-- Add a SearchResults component -->
+      						<!-- Add a Playlist component --> */}
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
+}
+
+export default App;
