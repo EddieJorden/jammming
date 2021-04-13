e898d4d1ab1d0344bc102e700343220c3360a2f1
Eddie Jorden <74274857+EddieJorden@users.noreply.github.com>
2021-04-12 22:55:20 -0700

    Spotify+api+integration (#9)
    * setup files css and components,
    
    * set up app, and created searchResults prop with mock searchresults for now, passed searchResults to SearchResults component
    
    * TrackList.js rendering <Track/> with .map(function) --ej--
    
    * here i have rendered the track name/album/artist in <Track/>
    
    * added mock user playlist -- whats up Var lml
    
    * added function to add track to playlist
    
    * added removeTrack() and passed it to onClick() in Track render()
    
    * Hot new features inlcude the ability to change the name of the playlist --ej-- whats up Var :)
    
    * functionality added to save user playlist to Spotify, however the Spotify API has not been set up yet, linter error is showing  however it will be used once the API has been integrated.
    
    * setup search bar to be connected to Spotify search
    
    * Spotify.js %99 done, however it is not connecting to Spotify, now the fun begins. maybe Var can show how me how to troubleshoot such things.
    
    * solved spotify api errror,  however still not showing results
    
    * found and fixed one error but still not functioning
    
    * working as much as its supposed to

---
+++ src/Components/App/App.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar.js';
+import SearchResults from '../SearchResults/SearchResults.js';
+import PlayList from '../PlayList/PlayList.js';
+
+import Spotify from '../../util/Spotify';
+
+Spotify.getAccessToken();
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.addTrack = this.addTrack.bind(this);
+		this.removeTrack = this.removeTrack.bind(this);
+		this.updatePlaylistName = this.updatePlaylistName.bind(this);
+		this.savePlaylist = this.savePlaylist.bind(this);
+		this.search = this.search.bind(this);
+
+		this.state = {
+			searchResults: [],
+			playlistName: 'playlistName',
+			playlistTracks: [],
+		};
+	}
+
+	search(term) {
+		Spotify.search(term).then((searchResults) => {
+			this.setState({ searchResults: searchResults });
+		});
+	}
+
+	addTrack(track) {
+		let tracks = this.state.playlistTracks;
+		if (
+			this.state.playlistTracks.find((savedTrack) => savedTrack.id === track.id)
+		) {
+			return;
+		}
+		tracks.push(track);
+		this.setState({ PlaylistTracks: tracks });
+	}
+
+	removeTrack(track) {
+		let tracks = this.state.playlistTracks;
+		tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
+		this.setState({ playlistTracks: tracks });
+	}
+
+	updatePlaylistName(name) {
+		this.setState({ playlistName: name });
+	}
+
+	savePlaylist() {
+		const trackUris = this.state.playlistTracks.map((track) => track.uri);
+		Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+			this.setState({ searchResults: [] });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>
+					Jam<span className="highlight">mm</span>ing
+				</h1>
+				<div className="App">
+					<SearchBar onSearch={this.search} />
+					<div className="App-playlist">
+						<SearchResults
+							onAdd={this.addTrack}
+							searchResults={this.state.searchResults}
+						/>
+						<PlayList
+							playListName={this.state.playlistName}
+							playlistTracks={this.state.playlistTracks}
+							onRemove={this.removeTrack}
+							onNameChange={this.updatePlaylistName}
+							onSave={this.savePlaylist}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
