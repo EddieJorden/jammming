3f7cc2f8b8c11c6f77a875a867a36fd907f242d1
EddieJorden <eddie@eddiejorden.tech>
2022-02-19 12:34:13 -0800

    Updates

---
+++ precache-manifest.df1af03a7bae5e09143bb6a68981aaf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19d4fed6b290f24b30336e4fb8a330b",
+    "url": "/my-jams/index.html"
+  },
+  {
+    "revision": "37e1973d83d326ad0720",
+    "url": "/my-jams/static/css/main.b9a01f05.chunk.css"
+  },
+  {
+    "revision": "262ccafda8e12d56eeea",
+    "url": "/my-jams/static/js/2.71f3c243.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/my-jams/static/js/2.71f3c243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12a5fcd72773b7b608b",
+    "url": "/my-jams/static/js/3.05f659a2.chunk.js"
+  },
+  {
+    "revision": "37e1973d83d326ad0720",
+    "url": "/my-jams/static/js/main.a1057297.chunk.js"
+  },
+  {
+    "revision": "18ff5381948d634bcb1b",
+    "url": "/my-jams/static/js/runtime-main.191a45a5.js"
+  },
+  {
+    "revision": "c4e558bd93169e771b5c768dfdf1ea4d",
+    "url": "/my-jams/static/media/music-background-desktop.c4e558bd.jpeg"
+  }
+]);
